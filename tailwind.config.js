9b23e13f77a0f152af0dfc6a0353e513cdee6f02
mohammedtakbir <takbirhossain678@gmail.com>
2022-11-12 18:39:03 +0600

    install tailwind, daisyUI, react-router-dom

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#0FCFEC",
+          "secondary": "#19D3AE",
+          "accent": "#3A4256",
+          "neutral": "#331E33",
+          "base-100": "#FFFFFF",
+        }
+      }
+    ]
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
